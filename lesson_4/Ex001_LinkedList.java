8300cbb168fc91030298be4146a92d09f246abe2
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-11 19:48:32 +0300

    add all exaple lesson4

---
+++ lesson_4/Ex001_LinkedList.java
@@ -0,0 +1,12 @@
+package lesson_4;
+
+import java.util.LinkedList;
+
+public class Ex001_LinkedList {
+  public static void main(String[] args) {
+    LinkedList<Integer> ll = new LinkedList<>();
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+  }
+}
---
+++ lesson_4/Ex003_Deque.java
@@ -0,0 +1,16 @@
+package lesson_4;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Ex003_Deque {
+  public static void main(String[] args) {
+    Deque<Integer> deque = new ArrayDeque<>();
+    deque.addFirst(1); deque.addLast(2);
+    deque.removeFirst(); deque.removeLast();
+    deque.offerFirst(1); deque.offerLast(2);
+    deque.pollFirst(); deque.pollLast();
+    deque.getFirst(); deque.getLast();
+    deque.peekFirst(); deque.peekLast();
+  }
+}
